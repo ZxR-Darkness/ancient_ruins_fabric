49f5ad7cd9449011b92f9e1cf7d0bff2a699d384
DARKNESS <imperium.foxes1@gmail.com>
2025-06-21 01:48:18 +0300

    added sword test

--- src/main/java/org/osteam/ancient_ruins/ModItems.java
+++ src/main/java/org/osteam/ancient_ruins/ModItems.java
@@ -10,12 +10,14 @@ import net.minecraft.registry.Registry;
 
 public class ModItems {
     public static final Item ANCIENT_SHARD = new Item(new Item.Settings());
+    public static final Item ANCIENT_SWORD = new Item(new Item.Settings());
     public static final Item FEX = new Item(new Item.Settings().food(new FoodComponent.Builder().build()));
 
 
     public static void register() {
         Registry.register(Registries.ITEM, "ancient_shard", ANCIENT_SHARD);
         Registry.register(Registries.ITEM, "fex", FEX);
+        Registry.register(Registries.ITEM, "ancient_sword", ANCIENT_SWORD);
 
     }
 
